823a0e57b57cf02537b25fb35346bb408cc97a25
lukoq <kinderlukasz01@gmail.com>
2023-05-03 15:48:00 +0200

    adding bottom sheet

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {StatusBar} from 'expo-status-bar';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Home from './screens/Home.js';
 import SignUpScreen from './screens/Signup.js';
 import LoginScreen from './screens/Login.js';
@@ -12,22 +13,24 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <PaperProvider>
-            <NavigationContainer>
-                <Stack.Navigator screenOptions={{headerShown: false}}>
-                    <Stack.Screen
-                        name="Login"
-                        component={LoginScreen}
-                    />
-                    <Stack.Screen
-                        name="Signup"
-                        component={SignUpScreen}
-                    />
-                    <Stack.Screen
-                        name="Home"
-                        component={Home}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </PaperProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <PaperProvider>
+                <NavigationContainer>
+                    <Stack.Navigator screenOptions={{headerShown: false}}>
+                        <Stack.Screen
+                            name="Login"
+                            component={LoginScreen}
+                        />
+                        <Stack.Screen
+                            name="Signup"
+                            component={SignUpScreen}
+                        />
+                        <Stack.Screen
+                            name="Home"
+                            component={Home}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </PaperProvider>
+       </GestureHandlerRootView>
     );
 }
