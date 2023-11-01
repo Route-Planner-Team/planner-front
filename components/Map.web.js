8bad7b5b25764e17d6a49a922d1f3c6fe00bec87
Jan Nowakowski <now.jan447@gmail.com>
2023-11-01 19:29:51 +0100

    Added a functional focus marker

--- components/Map.web.js
+++ components/Map.web.js
@@ -1,11 +1,13 @@
-import React, {useEffect, useImperativeHandle, useRef} from "react";
+import React, {useEffect, useImperativeHandle, useRef, useState} from "react";
 
-const DEFAULT_CENTER = { lat: 52.46672421135597, lng: 16.927230713146788 };
+const DEFAULT_CENTER = {lat: 52.46672421135597, lng: 16.927230713146788};
 const DEFAULT_ZOOM = 7;
 
 export const Map = React.forwardRef((props, ref) => {
     const mapRef = useRef(null);
     let mapInstanceRef = useRef(null);
+    let [focusMarker, setFocusMarker] = useState(null);
+    let markers = []
 
     useEffect(() => {
         if (mapRef.current) {
@@ -19,14 +21,25 @@ export const Map = React.forwardRef((props, ref) => {
 
     useImperativeHandle(ref, () => ({
         setFocus: (coordinates) => {
-            mapInstanceRef.current?.panTo(coordinates);
-        }
+            if (mapInstanceRef.current) {
+
+                // remove focus marker if exists
+                focusMarker?.setMap(null);
+
+                setFocusMarker(new window.google.maps.Marker({
+                    map: mapInstanceRef.current,
+                    position: coordinates
+                }));
+
+                mapInstanceRef.current.panTo(coordinates);
+            }
+        },
     }));
 
     return (
         <div
             ref={mapRef}
-            style={{ width: "100vw", height: "100vh" }}
+            style={{width: "100vw", height: "100vh"}}
         />
     );
 });
